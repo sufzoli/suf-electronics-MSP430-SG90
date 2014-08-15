03a453cf89f2a37047d6320fd0417323c7bb5cd0
suf <suf@freemail.hu>
2014-08-15 14:32:57 +0200

    Code cleanup

--- SW/SG90drv/main.c
+++ SW/SG90drv/main.c
@@ -7,29 +7,37 @@
 
 #include "serial.h"
 
-unsigned volatile char rxEchoPos;
+signed volatile char rxEchoPos;
 
 int checkSerial()
 {
 	int i;
 	int retvalue = -1;
+	// Iterate trough the received characters
 	for(;rxEchoPos<rxBuffLen;rxEchoPos++)
 	{
+		// if it is not an Enter
 		if(rxBuffer[rxEchoPos] != 13)
 		{
+			// Echo the char back
 			TimerA_UART_tx(rxBuffer[rxEchoPos]);
 		}
 		else
 		{
+			// Echo back correct Enter for the terminal
 			TimerA_UART_print("\r\n");
 			retvalue = 0;
+			// iterate trough the received characters
 			for(i = 0; i < rxBuffLen-1; i++)
 			{
+				// if it is a number
 				if(rxBuffer[i] > 47 && rxBuffer[i] < 58)
 				{
+					// add to the return value
 					retvalue = retvalue * 10 + rxBuffer[i] - 48;
 				}
 			}
+			// reset the receive buffer
 			rxEchoPos = -1;
 			rxBuffLen = 0;
 		}
@@ -40,6 +48,7 @@ int checkSerial()
 void moveServo(int deg)
 {
 	int pwm,k,i;
+	// generate PWM signal for the servo
 	pwm = 60 + deg;
 	for(k=0;k<50;k++)
 	{
@@ -50,6 +59,7 @@ void moveServo(int deg)
 		for(i=0;i<2000-pwm;i++)
 			__delay_cycles(125);
 	}
+	// send back an acknowledge to
 	TimerA_UART_print("ACK\r\n");
 }
 
@@ -64,55 +74,28 @@ void main (void)
 	// Startup Delay
 	_delay_cycles(2000);
 
+	// Initialize serial port
 	SERIAL_init();
+	// Enable interrupt
 	_EINT();
 	rxEchoPos = 0;
 
+	// Set the servo port to digital output
 	P2DIR |= BIT2;
 
- 	int i,j,k;
+ 	int deg; // servo rotation degree
 
- 	int deg;
-
- 	int pwm;
-
- 	// moveServo(0);
-
+ 	// Main loop
  	while(1)
  	{
- 		/*
- 		for(j=0;j<=180;j+=10)
- 		{
- 			moveServo(j);
- 		}
-		__delay_cycles(10000000);
-		*/
-
+ 		// load the target degree from the serial port
  		deg = checkSerial();
+ 		// if there is a new value
  		if(deg > -1)
  		{
+ 			// Move the servo
  			moveServo(deg);
  		}
  		__delay_cycles(1000);
- 		/*
- 		for(j=0;j<=180;j+=10)
- 		{
- 			pwm = 60 + j;
- 			for(k=0;k<50;k++)
- 			{
- 				P1OUT |= BIT4;
- 				for(i=0;i<pwm;i++)
- 					__delay_cycles(125);
- 				P1OUT &= ~BIT4;
- 				for(i=0;i<2000-pwm;i++)
- 					__delay_cycles(125);
- 				checkSerial();
- 			}
- 			__delay_cycles(1000);
- 		}
-
-		__delay_cycles(10000000);
-//		TimerA_UART_print("Cycle Finished\r\n");
- 		 */
  	}
 }
